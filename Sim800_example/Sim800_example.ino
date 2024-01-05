b98019a105805a575aafb016ee9fd16bd53ff0ef
Ziyad Tarek <zizoux07@gmail.com>
2024-01-05 14:52:01 +0200

    Update Sim800_example.ino

--- Sim800_example/Sim800_example.ino
+++ Sim800_example/Sim800_example.ino
@@ -31,11 +31,6 @@ TinyGsm        modem(SerialAT);
 
 
 void setup() {
-
-  /*
-     Initialize Serial and WiFi
-  */
-
   Serial.begin(115200);
   SerialAT.begin(9600);
 
@@ -64,77 +59,8 @@ void setup() {
   */
   mocpp_initialize(DEF_OCPP_URL, DEF_OCPP_ID, "a", "a");
 
-  /*
-     Integrate OCPP functionality. You can leave out the following part if your EVSE doesn't need it.
-  */
-  //  setEnergyMeterInput([]() {
-  //    //take the energy register of the main electricity meter and return the value in watt-hours
-  //    return 0.f;
-  //  });
-  //
-  //  setSmartChargingCurrentOutput([](float limit) {
-  //    //set the SAE J1772 Control Pilot value here
-  //    Serial.printf("[main] Smart Charging allows maximum charge rate: %.0f\n", limit);
-  //  });
-  //
-  //  setConnectorPluggedInput([]() {
-  //    //return true if an EV is plugged to this EVSE
-  //    return false;
-  //  });
-
-  //... see MicroOcpp.h for more settings
 }
 
 void loop() {
-
-  /*
-     Do all OCPP stuff (process WebSocket input, send recorded meter values to Central System, etc.)
-  */
   mocpp_loop();
-  //
-  //  /*
-  //     Energize EV plug if OCPP transaction is up and running
-  //  */
-  //  if (ocppPermitsCharge()) {
-  //    //OCPP set up and transaction running. Energize the EV plug here
-  //  } else {
-  //    //No transaction running at the moment. De-energize EV plug
-  //  }
-  //
-  //  /*
-  //     Use NFC reader to start and stop transactions
-  //  */
-  //  if (/* RFID chip detected? */ false) {
-  //    String idTag = "0123456789ABCD"; //e.g. idTag = RFID.readIdTag();
-  //
-  //    if (!getTransaction()) {
-  //      //no transaction running or preparing. Begin a new transaction
-  //      Serial.printf("[main] Begin Transaction with idTag %s\n", idTag.c_str());
-  //
-  //      /*
-  //         Begin Transaction. The OCPP lib will prepare transaction by checking the Authorization
-  //         and listen to the ConnectorPlugged Input. When the Authorization succeeds and an EV
-  //         is plugged, the OCPP lib will send the StartTransaction
-  //      */
-  //      auto ret = beginTransaction(idTag.c_str());
-  //
-  //      if (ret) {
-  //        Serial.println(F("[main] Transaction initiated. OCPP lib will send a StartTransaction when" \
-  //                         "ConnectorPlugged Input becomes true and if the Authorization succeeds"));
-  //      } else {
-  //        Serial.println(F("[main] No transaction initiated"));
-  //      }
-  //
-  //    } else {
-  //      //Transaction already initiated. Check if to stop current Tx by RFID card
-  //      if (idTag.equals(getTransactionIdTag())) {
-  //        //card matches -> user can stop Tx
-  //        Serial.println(F("[main] End transaction by RFID card"));
-  //
-  //        endTransaction(idTag.c_str());
-  //      } else {
-  //        Serial.println(F("[main] Cannot end transaction by RFID card (different card?)"));
-  //      }
-  //    }
-  //  }
 }
